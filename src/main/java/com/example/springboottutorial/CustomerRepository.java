69d96fd326e5f280ec9b38b04d7261c8a965b222
schragman <schragman@gmail.com>
2022-02-03 17:59:05 +0100

    Nur zum Backup

---
+++ src/main/java/com/example/springboottutorial/CustomerRepository.java
@@ -0,0 +1,9 @@
+package com.example.springboottutorial;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CustomerRepository extends CrudRepository<Customer, Integer> {
+
+  Customer findCustomerById(Integer id);
+
+}
